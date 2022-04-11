7fe1e2bc922cabfffefcac8d1c2851bd73ec2808
maxts0gt <bazarsadtsogtjargal@gmail.com>
2022-04-11 23:35:55 +0900

    initial setup

---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import Text from "./Text";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Text />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
