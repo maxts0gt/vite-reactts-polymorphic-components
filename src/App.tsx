3f736bf72526906461bf23446cd6a68403de132e
maxts0gt <bazarsadtsogtjargal@gmail.com>
2022-04-12 00:52:58 +0900

    adding styles as props with type check

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,10 @@ export default function App() {
   return (
     <div className="App">
       <Text as="h1">Hello Polymorphic Components!</Text>
-      <Text as="h2"> Let's master you!</Text>
+      <Text as="h2" color="teal" style={{ backgroundColor: "black" }}>
+        {" "}
+        Let's master you!
+      </Text>
       <Text as="a" href="https://google.com">
         Nothing will show span!
       </Text>
--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -1,15 +1,30 @@
 import React from "react";
 
+type randomColors = "yellow" | "orange" | "green" | "teal";
+
 type TextProps<T extends React.ElementType> = {
   as?: T;
-  children: React.ReactNode;
-} & React.ComponentPropsWithoutRef<T>;
+  color?: randomColors | "black";
+};
+
+type Props<T extends React.ElementType> = React.PropsWithChildren<
+  TextProps<T>
+> &
+  Omit<React.ComponentPropsWithoutRef<T>, keyof TextProps<T>>;
 
 export const Text = <T extends React.ElementType = "span">({
   as,
+  style,
+  color,
   children,
   ...restProps
-}: TextProps<T>) => {
+}: Props<T>) => {
   const Component = as || "span";
-  return <Component {...restProps}>{children}</Component>;
+
+  const internalStyles = color ? { style: { ...style, color } } : {};
+  return (
+    <Component {...restProps} {...internalStyles}>
+      {children}
+    </Component>
+  );
 };
